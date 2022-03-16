3424ec531065702d7db2b6132585ece7d846b17c
Danitza Bonilla Fandiño <danitza.bonilla@masivian.com>
2022-03-16 13:55:12 -0500

    add components

---
+++ src/components/Ranking.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="card-ranking p-5">
+    <div
+      class="
+        font-caveat-brush
+        has-text-primary
+        is-size-3
+        has-text-weight-bold has-text-centered
+        mt-3
+      "
+    >
+      Top {{ top }} - Mejores Comic
+    </div>
+
+    <div v-for="(item, index) in ranking" :key="index">
+      <v-divider v-if="index !== 0" class=""></v-divider>
+      <div class="m-4">
+        <div class="columns is-mobile">
+          <div class="column is-4">
+            <v-badge
+              :content="`#${index + 1}`"
+              :left="true"
+              color="primary"
+            >
+              <v-img
+                max-width="100"
+                max-height="400"
+                :src="item.img"
+                :alt="item.alt"
+              ></v-img>
+            </v-badge>
+          </div>
+          <div class="column">
+            <div class="is-size-5 has-text-weight-semibold">
+              {{ item.title }}
+            </div>
+            <div class="">{{ item.year }}</div>
+            <div class="has-text-yellow has-text-weight-semibold">
+              <span class="is-size-5">{{
+                parseFloat(item.rating).toFixed(1)
+              }}</span>
+              /
+              <span class="is-size-6">5.0</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import { types } from '@/store/module/comicRating/types';
+
+export default {
+  data() {
+    return {
+      top: 3,
+      ranking: [],
+    };
+  },
+  computed: {
+    ...mapState(types.PATH, ['ratings']),
+  },
+  watch: {
+    ratings: function watchRatings(val) {
+      this.updateRanking(val);
+    },
+  },
+  methods: {
+    updateRanking(data) {
+      this.ranking = Object.values(data)
+        .sort((a, b) => b.rating - a.rating)
+        .slice(0, this.top);
+    },
+  },
+};
+</script>
---
+++ src/components/Rating.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-rating
+    v-model="rating"
+    color="yellow darken-3"
+    background-color="grey darken-1"
+    empty-icon="$ratingFull"
+    half-increments
+    hover
+    :size="size"
+  ></v-rating>
+</template>
+
+<script>
+export default {
+  props: {
+    value: { type: Number, required: true },
+    size: { type: String, default: () => '30' },
+  },
+  computed: {
+    rating: {
+      get() {
+        return this.value;
+      },
+      set(val) {
+        this.$emit('input', val);
+      },
+    },
+  },
+};
+</script>
